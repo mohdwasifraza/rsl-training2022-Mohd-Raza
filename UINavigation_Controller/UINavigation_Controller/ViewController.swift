e7af0de75f38fcfbe0ff6d1c854f1037e711fc8c
Mohd Wasif Raza <mohd.raza@rsllearning.com>
2022-05-10 20:44:27 +0530

    Day 16: UI_Navigation_Controller Assignment
    Completed UINavigationController Assignment

---
+++ UINavigation_Controller/UINavigation_Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UINavigation_Controller
+//
+//  Created by Mohd Raza on 10/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //title = "bar items"
+        view.backgroundColor = .systemPink
+        //navigationItem.hidesBackButton = true
+        
+        
+    }
+
+
+}
+
